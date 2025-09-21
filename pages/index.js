d3c645f7b07bd3483782237b66c67df5b564d34a
lucasaf <lucasfraga.dev@gmail.com>
2025-09-21 04:13:23 -0300

    adiciona scripts `lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home () {
-  return <h1>Paloma, você é o amor da minha vida!</h1>
+function Home() {
+  return <h1>Paloma, você é o amor da minha vida!</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+function teste2() {
+  console.log("indentacao errada");
+}
 export default Home;
