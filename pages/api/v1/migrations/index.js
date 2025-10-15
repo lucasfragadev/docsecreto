6d639b636406b83db18b32c31138bacd4a87365d
LucasAF <lucasfraga.dev@gmail.com>
2025-10-14 23:05:09 -0300

    adds migration scripts and corresponding API tests

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,14 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+
+export default async function migrations(req, res) {
+  const migrations = await migrationRunner({
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pg.migrations",
+  });
+  res.status(200).json(migrations);
+};
